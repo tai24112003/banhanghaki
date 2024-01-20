f86d05379f8b0341002bde7e813ba70a6d9d90ad
unknown <anigaming1994@gmail.com>
2024-01-20 22:47:56 +0700

    add firebase + clound messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,32 @@
-import 'package:bangiayhaki/components/ListArmchair.dart';
-import 'package:bangiayhaki/components/ListBed.dart';
-import 'package:bangiayhaki/components/ListChari.dart';
-import 'package:bangiayhaki/components/ListLamp.dart';
-import 'package:bangiayhaki/views/AdminProfileScreen.dart';
-import 'package:bangiayhaki/views/HomeScreen.dart';
 import 'package:bangiayhaki/views/LoginScreen.dart';
-import 'package:bangiayhaki/views/NotiScreen.dart';
-import 'package:bangiayhaki/views/OrderOfCus.dart';
-import 'package:bangiayhaki/views/ProductsManageScreen.dart';
-import 'package:bangiayhaki/views/ProfileScreen.dart';
-import 'package:bangiayhaki/views/RegisterScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class GlobalVariable {
-  static final GlobalVariable _instance = GlobalVariable._internal();
-
-  factory GlobalVariable() => _instance;
-
-  GlobalVariable._internal();
-
-  String myVariable =
-      'https://c797-2402-800-63b7-d74a-f545-3330-10f-15a0.ngrok-free.app';
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
